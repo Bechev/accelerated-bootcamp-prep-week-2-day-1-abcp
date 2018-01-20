d75be8e320b1da345894d574fd51dca7d0775ab1
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-20 17:25:17 +0000

    automatically backed up by learn

--- dom.js
+++ dom.js
@@ -45,7 +45,7 @@ var document = [
 
 var childrenArray =[];
 document.forEach(function getChildren(element, index){
-  childrenArray.push(document.children[index]) ;
+  childrenArray.push(document.children) ;
 })
 
 console.log(childrenArray);
